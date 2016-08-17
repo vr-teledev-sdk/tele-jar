4eeb1f3628622850573e78e814f2a9a093b72813
Alexei Korolyov <korolyov@yandex-team.ru>
2016-08-17 23:48:24 +0300

    example unity app

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    public Text scoreText;
+    public TelegramListener TelegramListener;
+
+    private int score = 0;
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        TelegramListener.RepaintEvent.AddListener(color => GetComponent<MeshRenderer>().material.color = color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate () {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        rigidBody.AddForce(horiz * speed, 0, vert * speed);
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.GetComponent<PickupObjectBehaviour>().Disappear();
+        scoreText.text = "Score: " + ++score;
+    }
+}
